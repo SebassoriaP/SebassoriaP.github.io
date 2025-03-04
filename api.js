0689694b6c242da1f8e00561fa950ff8c0d82ac3
SebassoriaP <workssssorip@gmail.com>
2025-03-04 12:48:20 -0600

    Add files via upload

---
+++ api.js
@@ -0,0 +1,19 @@
+let image;
+
+async function API() {
+    const apiKey = "NZB3h5xLCRiHMkjKU2Dmdr3OAxmGaAbtn6LbqW2f";
+    const url = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+
+    const data = await url.json(); 
+    console.log(data);  
+
+    image = data.hdurl;
+    console.log(image);
+
+    let s=document.createElement("img");
+    s.src=image;
+    document.body.appendChild(s);
+
+    
+}
+

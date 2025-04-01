c4ad75a7736d60217b76b661ef54f17daf966392
SebassoriaP <workssssorip@gmail.com>
2025-04-01 08:18:48 -0600

    comentarios api_srvr.js

--- api_srvr.js
+++ api_srvr.js
@@ -11,6 +11,10 @@ server.listen(1985, () => {
 let title;
 
 async function API() {
+    //Aunque esta API si requiere de un token, consideré que no había problema de ponerla pública (aunque no es lo adecuado) porque realmente 
+    //no se le puede dar un uso malicioso, ya que, como tal a la página de la NASA realmente solo se usa para trabajar con API's pero 
+    //no contiene tu información personal o algo con lo que podrían hacer daño
+    //link de la Nasa: https://api.nasa.gov/
     const apiKey = "bbI7e6MhlgLaNWe3h6lEDgWjURFsAzbEFQMtB9Rk";
     const url = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
 
